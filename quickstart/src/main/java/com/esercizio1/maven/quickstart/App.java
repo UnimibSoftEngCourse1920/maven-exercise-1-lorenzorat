c32431ed7d31f49d04298dbd11e7ee43c2b6493e
Lorenzo Zorat <l.zorat@campus.unimib.it>
2019-10-29 10:55:45 +0100

    primo commit

---
+++ quickstart/src/main/java/com/esercizio1/maven/quickstart/App.java
@@ -0,0 +1,12 @@
+package com.esercizio1.maven.quickstart;
+
+import com.google.gson.Gson;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Gson gson = new Gson();
+        System.out.println(gson.toJson("Hello World!") );
+    }
+}
